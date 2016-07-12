c62fc4b609af25f0fb0e21853890a2354d69cc45
dhruv chauhan <dc14690@my.bristol.ac.uk>
2016-07-12 11:45:39 +0100

    at some strange middle stage between migration

---
+++ app/nerds.js
@@ -0,0 +1,129 @@
+//this is where CRUD would be done / include API routes in future
+
+//get nerd model
+var Nerd = require('./models/nerd');
+
+var express = require('express');
+var router = express.Router();
+
+router.use(function(req, res, next) {
+    console.log("communicating with API");
+    next();
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to our api!' });
+});
+
+//routes for /nerds ======
+router.route('/nerds')
+
+    //make a nerd (@ POST localhost:8080/api/nerds)
+    .post(function(req, res) {
+
+        var nerd = new Nerd(); //new nerd model instance
+        nerd.name = req.body.name; //set nerd name
+
+        //save nerd and check for errors
+        nerd.save(function(err) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json({ message: 'Nerd created!' });
+        });
+    })
+
+    //get all nerds @ localhost:8080/api/nerds
+    .get(function(req, res) {
+        Nerd.find(function(err, nerds) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(nerds);
+        });
+    });
+
+//routes for /nerds/:nerd_id ===
+router.route('/nerds/:nerd_id')
+
+    //get nerd with id @ localhost:8080/api/nerds/:nerd_id
+    .get(function(req, res) {
+        Nerd.findById(req.params.nerd_id, function(err, nerd) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(nerd);
+        });
+    })
+
+    //update nerd with id PUT @ localhost:8080/api/nerds/:nerd_id
+    .put(function(req, res) {
+
+        //use nerd model to find nerd
+        Nerd.findById(req.params.nerd_id, function(err, nerd) {
+
+            if (err) {
+                res.send(err);
+            }
+
+            nerd.name = req.body.name; // update the nerd info
+
+            // save nerd
+            nerd.save(function(err) {
+                if (err) {
+                    res.send(err);
+                }
+
+                res.json({ message: 'Nerd updated!' });
+            });
+        });
+
+    })
+
+    //delete nerd with id @ DELETE localhost:8080/api/nerds/:nerd_id
+    .delete(function(req, res) {
+        Nerd.remove({
+            _id: req.params.nerd_id
+        }, function(err, nerd) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json({message: 'sucessfully deleted' });
+        });
+    });
+
+
+module.exports = router;
+//server routes ===
+// handle api calls, authentication routes etc
+
+//example api route
+// app.get('/api/nerds', function(req, res) {
+//     //use mongoose to get all 'Nerds' in db
+//     Nerd.find(function(err, nerds) {
+//
+//         //if error, send the error
+//         //nothing after res.send(err) executes
+//
+//         if (err)
+//             res.send(err);
+//
+//         res.json(nerds) // return in JSON format
+//     });
+// });
+
+// add route to handle creating (app.post)
+
+// add route to handle delete (app.delete)
+
+// //frontend routes ===
+// //route to handle angular requests
+// //catch-all route
+// app.get('*', function(req, res) {
+// //perhaps should be sendFile? note if doesn't work
+//     res.sendfile('./public/views/index.html'); // loads public/index.html file to route url
+// });
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var mongoose        = require('mongoose');
 mongoose.connect('mongodb://dhruv:amr@ds015995.mlab.com:15995/cowllection'); //connect to database
 
 var Nerd            = require('./app/models/nerd');
+var nerds           = require('./app/nerds');
 
 //config ===
 
@@ -39,111 +40,121 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
 
 // routes === move routes to below doc once discrepancies are sorted?
-// require('./app/routes')(app);
+ // require('./app/routes')(app);
+app.use('/api/nerds', nerds);
+
 
 //routes for api ===
-var router = express.Router(); // get instance of express Router
-
-//middleware for all reqs
-router.use(function(req, res, next) {
-    //log
-    console.log("communicating with API");
-    next(); //go to next routes
-});
-
-//test route to see if working
-//at GET http://localhost:8080/api
-router.get('/', function(req, res) {
-    //sending back as json
-    res.json({ message: 'hooray! welcome to our api!' });
-});
-
-//add more api magic below
-
-//routes for /nerds === ===
-router.route('/nerds')
-
-    //make a nerd (@ POST localhost:8080/api/nerds)
-    .post(function(req, res) {
-
-        var nerd = new Nerd(); //new nerd model instance
-        nerd.name = req.body.name; //set nerd name
-
-        //save nerd and check for errors
-        nerd.save(function(err) {
-            if (err) {
-                res.send(err);
-            }
-
-            res.json({ message: 'Nerd created!' });
-        });
-    })
-
-    //get all nerds @ localhost:8080/api/nerds
-    .get(function(req, res) {
-        Nerd.find(function(err, nerds) {
-            if (err) {
-                res.send(err);
-            }
-
-            res.json(nerds);
-        });
-    });
-
-//routes for /nerds/:nerd_id ===
-router.route('/nerds/:nerd_id')
-
-    //get nerd with id @ localhost:8080/api/nerds/:nerd_id
-    .get(function(req, res) {
-        Nerd.findById(req.params.nerd_id, function(err, nerd) {
-            if (err) {
-                res.send(err);
-            }
-
-            res.json(nerd);
-        });
-    })
-
-    //update nerd with id PUT @ localhost:8080/api/nerds/:nerd_id
-    .put(function(req, res) {
-
-        //use nerd model to find nerd
-        Nerd.findById(req.params.nerd_id, function(err, nerd) {
-
-            if (err) {
-                res.send(err);
-            }
-
-            nerd.name = req.body.name; // update the nerd info
-
-            // save nerd
-            nerd.save(function(err) {
-                if (err) {
-                    res.send(err);
-                }
-
-                res.json({ message: 'Nerd updated!' });
-            });
-        });
-
-    })
-
-    //delete nerd with id @ DELETE localhost:8080/api/nerds/:nerd_id
-    .delete(function(req, res) {
-        Nerd.remove({
-            _id: req.params.nerd_id
-        }, function(err, nerd) {
-            if (err) {
-                res.send(err);
-            }
-
-            res.json({message: 'sucessfully deleted' });
-        });
-    });
+// var router = express.Router(); // get instance of express Router
+//
+// //middleware for all reqs
+// router.use(function(req, res, next) {
+//     //log
+//     console.log("communicating with API");
+//     next(); //go to next routes
+// });
+//
+// //test route to see if working
+// //at GET http://localhost:8080/api
+// router.get('/', function(req, res) {
+//     //sending back as json
+//     res.json({ message: 'hooray! welcome to our api!' });
+// });
+//
+// //add more api magic below
+//
+// //routes for /nerds === ===
+// router.route('/nerds')
+//
+//     //make a nerd (@ POST localhost:8080/api/nerds)
+//     .post(function(req, res) {
+//
+//         var nerd = new Nerd(); //new nerd model instance
+//         nerd.name = req.body.name; //set nerd name
+//
+//         //save nerd and check for errors
+//         nerd.save(function(err) {
+//             if (err) {
+//                 res.send(err);
+//             }
+//
+//             res.json({ message: 'Nerd created!' });
+//         });
+//     })
+//
+//     //get all nerds @ localhost:8080/api/nerds
+//     .get(function(req, res) {
+//         Nerd.find(function(err, nerds) {
+//             if (err) {
+//                 res.send(err);
+//             }
+//
+//             res.json(nerds);
+//         });
+//     });
+//
+// //routes for /nerds/:nerd_id ===
+// router.route('/nerds/:nerd_id')
+//
+//     //get nerd with id @ localhost:8080/api/nerds/:nerd_id
+//     .get(function(req, res) {
+//         Nerd.findById(req.params.nerd_id, function(err, nerd) {
+//             if (err) {
+//                 res.send(err);
+//             }
+//
+//             res.json(nerd);
+//         });
+//     })
+//
+//     //update nerd with id PUT @ localhost:8080/api/nerds/:nerd_id
+//     .put(function(req, res) {
+//
+//         //use nerd model to find nerd
+//         Nerd.findById(req.params.nerd_id, function(err, nerd) {
+//
+//             if (err) {
+//                 res.send(err);
+//             }
+//
+//             nerd.name = req.body.name; // update the nerd info
+//
+//             // save nerd
+//             nerd.save(function(err) {
+//                 if (err) {
+//                     res.send(err);
+//                 }
+//
+//                 res.json({ message: 'Nerd updated!' });
+//             });
+//         });
+//
+//     })
+//
+//     //delete nerd with id @ DELETE localhost:8080/api/nerds/:nerd_id
+//     .delete(function(req, res) {
+//         Nerd.remove({
+//             _id: req.params.nerd_id
+//         }, function(err, nerd) {
+//             if (err) {
+//                 res.send(err);
+//             }
+//
+//             res.json({message: 'sucessfully deleted' });
+//         });
+//     });
 
 //register routes ===
 // all routes are prefixed with /api
-app.use('/api', router);
+// app.use('/api', router);
+//
+// //frontend routes ===
+// //route to handle angular requests
+// //catch-all route
+// app.get('*', function(req, res) {
+// //perhaps should be sendFile? note if doesn't work
+//     res.sendfile('./public/views/index.html'); // loads public/index.html file to route url
+// });
 
 //start app ===
 //at http://localhost:8080
