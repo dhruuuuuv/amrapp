bc052f1029f590d0d97c50778a00ed2653654fa0
dhruv chauhan <dc14690@my.bristol.ac.uk>
2016-07-12 12:01:13 +0100

    working still

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // url: 'mongodb://localhost/stencil-dev'
     url: 'mongodb://dhruv:amr@ds015995.mlab.com:15995/cowllection'
 }
--- server.js
+++ server.js
@@ -5,9 +5,8 @@ var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 var mongoose        = require('mongoose');
 
-mongoose.connect('mongodb://dhruv:amr@ds015995.mlab.com:15995/cowllection'); //connect to database
 
-// var Nerd            = require('./app/models/nerd');
+//import routing from app
 var nerds           = require('./app/nerds');
 
 //config ===
@@ -15,15 +14,14 @@ var nerds           = require('./app/nerds');
 //config files
 var db = require('./config/db');
 
+//connect to mongodb
+mongoose.connect(db.url)
+
 //set port
 var port = process.env.PORT || 8080;
 
-//connect to mongodb
-//todo uncomment when credentials entered in config/db.js
-//mongoose.connect(db.url)
 
 //get data of the body / POST parameters
-
 //parse app/json
 app.use(bodyParser.json());
 
@@ -39,115 +37,9 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 //set static files location, eg /public/img is /img for users
 app.use(express.static(__dirname + '/public'));
 
-// routes === move routes to below doc once discrepancies are sorted?
- // require('./app/routes')(app);
+// routes ===
 app.use('/api/nerds', nerds);
 
-
-//routes for api ===
-// var router = express.Router(); // get instance of express Router
-//
-// //middleware for all reqs
-// router.use(function(req, res, next) {
-//     //log
-//     console.log("communicating with API");
-//     next(); //go to next routes
-// });
-//
-// //test route to see if working
-// //at GET http://localhost:8080/api
-// router.get('/', function(req, res) {
-//     //sending back as json
-//     res.json({ message: 'hooray! welcome to our api!' });
-// });
-//
-// //add more api magic below
-//
-// //routes for /nerds === ===
-// router.route('/nerds')
-//
-//     //make a nerd (@ POST localhost:8080/api/nerds)
-//     .post(function(req, res) {
-//
-//         var nerd = new Nerd(); //new nerd model instance
-//         nerd.name = req.body.name; //set nerd name
-//
-//         //save nerd and check for errors
-//         nerd.save(function(err) {
-//             if (err) {
-//                 res.send(err);
-//             }
-//
-//             res.json({ message: 'Nerd created!' });
-//         });
-//     })
-//
-//     //get all nerds @ localhost:8080/api/nerds
-//     .get(function(req, res) {
-//         Nerd.find(function(err, nerds) {
-//             if (err) {
-//                 res.send(err);
-//             }
-//
-//             res.json(nerds);
-//         });
-//     });
-//
-// //routes for /nerds/:nerd_id ===
-// router.route('/nerds/:nerd_id')
-//
-//     //get nerd with id @ localhost:8080/api/nerds/:nerd_id
-//     .get(function(req, res) {
-//         Nerd.findById(req.params.nerd_id, function(err, nerd) {
-//             if (err) {
-//                 res.send(err);
-//             }
-//
-//             res.json(nerd);
-//         });
-//     })
-//
-//     //update nerd with id PUT @ localhost:8080/api/nerds/:nerd_id
-//     .put(function(req, res) {
-//
-//         //use nerd model to find nerd
-//         Nerd.findById(req.params.nerd_id, function(err, nerd) {
-//
-//             if (err) {
-//                 res.send(err);
-//             }
-//
-//             nerd.name = req.body.name; // update the nerd info
-//
-//             // save nerd
-//             nerd.save(function(err) {
-//                 if (err) {
-//                     res.send(err);
-//                 }
-//
-//                 res.json({ message: 'Nerd updated!' });
-//             });
-//         });
-//
-//     })
-//
-//     //delete nerd with id @ DELETE localhost:8080/api/nerds/:nerd_id
-//     .delete(function(req, res) {
-//         Nerd.remove({
-//             _id: req.params.nerd_id
-//         }, function(err, nerd) {
-//             if (err) {
-//                 res.send(err);
-//             }
-//
-//             res.json({message: 'sucessfully deleted' });
-//         });
-//     });
-
-//register routes ===
-// all routes are prefixed with /api
-// app.use('/api', router);
-//
 //frontend routes ===
 //route to handle angular requests
 //catch-all route
