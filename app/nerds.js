286e44b4154af4b28ef1f6c1787f44d2212c3ca0
dhruv chauhan <dc14690@my.bristol.ac.uk>
2016-07-12 12:04:21 +0100

    nerd server route successfully refactored into app/nerds.js

--- app/nerds.js
+++ app/nerds.js
@@ -98,32 +98,3 @@ router.route('/:nerd_id')
 
 
 module.exports = router;
-//server routes ===
-// handle api calls, authentication routes etc
-
-//example api route
-// app.get('/api/nerds', function(req, res) {
-//     //use mongoose to get all 'Nerds' in db
-//     Nerd.find(function(err, nerds) {
-//
-//         //if error, send the error
-//         //nothing after res.send(err) executes
-//
-//         if (err)
-//             res.send(err);
-//
-//         res.json(nerds) // return in JSON format
-//     });
-// });
-
-// add route to handle creating (app.post)
-
-// add route to handle delete (app.delete)
-
-// //frontend routes ===
-// //route to handle angular requests
-// //catch-all route
-// app.get('*', function(req, res) {
-// //perhaps should be sendFile? note if doesn't work
-//     res.sendfile('./public/views/index.html'); // loads public/index.html file to route url
-// });
