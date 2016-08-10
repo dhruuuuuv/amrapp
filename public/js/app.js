9bddcede092df73e79e8d6ab068855c07ff5ac97
dhruv chauhan <dc14690@my.bristol.ac.uk>
2016-08-10 12:02:22 +0100

    tried to connect fe and be

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'FarmCtrl', 'FarmService'])
-.controller('db', ['$scope', 'Farm', function($scope, Farm) {
-    Farm.get().then(function(farms) {
-        $scope.farms = farms;
-    });
-}]);
+// angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'FarmCtrl', 'FarmService'])
+// .controller('db', ['$scope', 'Farm', function($scope, Farm) {
+//     Farm.get().then(function(farms) {
+//         $scope.farms = farms;
+//     });
+// }]);
+
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'FarmCtrl', 'FarmService']);
