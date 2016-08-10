2948ac5d7d246be47f855716f7c80e045763a053
dhruv chauhan <dc14690@my.bristol.ac.uk>
2016-08-10 13:56:47 +0100

    refactoring format

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-// angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'FarmCtrl', 'FarmService'])
-// .controller('db', ['$scope', 'Farm', function($scope, Farm) {
-//     Farm.get().then(function(farms) {
-//         $scope.farms = farms;
-//     });
-// }]);
-
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'FarmCtrl', 'FarmService']);
--- public/js/controllers/FarmCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('FarmCtrl', []).controller('FarmController', [
-    '$scope',
-    'farms',
-    'farm',
-     function($scope, farms) {
-        $scope.tagline = 'I am a test farm.';
-        $scope.farm = farm;
-    }
-]);
